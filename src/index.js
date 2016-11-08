201d5583d75150a0c90204fbf99a7353fc32df2e
Alec Hirsch <alec.hirsch@anchorops.com>
2016-11-08 16:19:41 -0500

    Copied the logic for equality filter

--- src/index.js
+++ src/index.js
@@ -322,10 +322,7 @@ export default (Bookshelf, options = {}) => {
                                     // Determine if there are multiple filters to be applied
                                     let valueArray = null;
                                     if (!_isArray(typeValue)){
-                                        console.log(typeValue);
-                                        console.log(typeof typeValue);
                                         valueArray = split(typeValue.toString(), ',');
-                                        console.log(valueArray);
                                     }
                                     else {
                                         valueArray = typeValue;
@@ -394,7 +391,9 @@ export default (Bookshelf, options = {}) => {
                                 key = internals.formatRelation(internals.formatColumnNames([key])[0]);
 
                                 // Determine if there are multiple filters to be applied
-                                value = value.toString().indexOf(',') !== -1 ? value.split(',') : value;
+                                if (!isArray(value)){
+                                    value = split(value.toString(), ',');
+                                }
                                 qb.whereIn(key, value);
                             }
                         }
